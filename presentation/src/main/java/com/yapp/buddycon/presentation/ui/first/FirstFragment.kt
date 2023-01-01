c899a398eaa360a1a8319b41e14e196d1e39bbe1
ows3090 <ows3090@naver.com>
2023-01-01 17:08:04 +0900

    [Feat] #6 Navigation Graph 및 Bottom Navigation 기본 세팅

---
+++ presentation/src/main/java/com/yapp/buddycon/presentation/ui/first/FirstFragment.kt
@@ -0,0 +1,14 @@
+package com.yapp.buddycon.presentation.ui.first
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.yapp.buddycon.presentation.R
+import com.yapp.buddycon.presentation.base.BaseFragment
+import com.yapp.buddycon.presentation.databinding.FragmentFirstBinding
+import timber.log.Timber
+
+class FirstFragment : BaseFragment<FragmentFirstBinding>(R.layout.fragment_first) {
+}
