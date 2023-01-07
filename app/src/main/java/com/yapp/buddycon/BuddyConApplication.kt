2ac0ec55742e1b9042789a91619d3a365185ec4a
ows3090 <ows3090@naver.com>
2023-01-08 02:47:05 +0900

    [Feat]: KaKaoLogin 버튼 클릭 시 로직 구현

--- app/src/main/java/com/yapp/buddycon/BuddyConApplication.kt
+++ app/src/main/java/com/yapp/buddycon/BuddyConApplication.kt
@@ -2,10 +2,16 @@ package com.yapp.buddycon
 
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
+import timber.log.Timber
 
 class BuddyConApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+
         KakaoSdk.init(applicationContext, BuildConfig.KAKAO_APP_KEY)
     }
 }
--- presentation/src/main/java/com/yapp/buddycon/presentation/ui/login/KakaoLoginActivity.kt
+++ presentation/src/main/java/com/yapp/buddycon/presentation/ui/login/KakaoLoginActivity.kt
@@ -2,10 +2,58 @@ package com.yapp.buddycon.presentation.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
 import com.yapp.buddycon.presentation.R
 import com.yapp.buddycon.presentation.base.BaseActivity
 import com.yapp.buddycon.presentation.databinding.ActivityKakaoLoginBinding
+import timber.log.Timber
 
 class KakaoLoginActivity : BaseActivity<ActivityKakaoLoginBinding>(R.layout.activity_kakao_login) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
+        binding.btnKakakoLogin.setOnClickListener {
+            startKaKaoLogin()
+        }
+    }
+
+    /**
+     * @Description 카카오톡 설치되어 있으면 카카오톡으로 로그인, 없으면 카카오계정으로 로그인
+     */
+    private fun startKaKaoLogin() {
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+            UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+                error?.let { e ->
+                    Timber.e(getString(R.string.kakao_login_error, e))
+
+                    // 카카오톡 설치 이후 취소한 경우
+                    if (e is ClientError && e.reason == ClientErrorCause.Cancelled) {
+                        return@loginWithKakaoTalk
+                    }
+
+                    UserApiClient.instance.loginWithKakaoAccount(this, callback = getKaKaoAccountApiCallback())
+                } ?: kotlin.run {
+                    // TODO 성공 처리
+                    Timber.d(getString(R.string.kakao_login_success, token))
+                }
+            }
+        } else {
+            UserApiClient.instance.loginWithKakaoAccount(this, callback = getKaKaoAccountApiCallback())
+        }
+    }
+
+    private fun getKaKaoAccountApiCallback(): (OAuthToken?, Throwable?) -> Unit = { token, error ->
+        error?.let { e ->
+            Timber.e(getString(R.string.kakao_login_error, e))
+        } ?: kotlin.run {
+            token?.let {
+                // TODO 성공 처리
+                Timber.d(getString(R.string.kakao_login_success, token))
+            }
+        }
+    }
 }
