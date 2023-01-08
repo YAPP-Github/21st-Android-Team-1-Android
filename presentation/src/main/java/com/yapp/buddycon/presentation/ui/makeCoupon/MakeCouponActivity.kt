a5c2f36f87a287440b62c39a31b58e0334eae428
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2023-01-08 14:29:37 +0900

    [Resource] appbar의 오른쪽 상단 버튼 추가
    - 기본 값은 VISEIBLE = GONE

--- presentation/src/main/java/com/yapp/buddycon/presentation/ui/makeCoupon/MakeCouponActivity.kt
+++ presentation/src/main/java/com/yapp/buddycon/presentation/ui/makeCoupon/MakeCouponActivity.kt
@@ -1,6 +1,7 @@
 package com.yapp.buddycon.presentation.ui.makeCoupon
 
 import android.os.Bundle
+import android.view.View
 import com.yapp.buddycon.presentation.R
 import com.yapp.buddycon.presentation.base.BaseActivity
 import com.yapp.buddycon.presentation.databinding.ActivityMakeCouponBinding
@@ -8,6 +9,7 @@ import com.yapp.buddycon.presentation.databinding.ActivityMakeCouponBinding
 class MakeCouponActivity : BaseActivity<ActivityMakeCouponBinding>(R.layout.activity_make_coupon) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.appbarMakeCoupon.tvAppbarRight.visibility = View.VISIBLE
         binding.appbarMakeCoupon.ibnAppbarBack.setOnClickListener { finish() }
     }
 }
