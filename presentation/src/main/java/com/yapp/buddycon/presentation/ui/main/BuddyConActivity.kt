e89439631aeb2228e60ac9111042432f2215b929
한창희 <nimok97@gmail.com>
2023-01-08 20:16:43 +0900

    [Fix] BuddyConActivity 수정 '쿠폰 등록하기' 선택하는 경우에만 바로 갤러리 이동

--- presentation/src/main/java/com/yapp/buddycon/presentation/ui/main/BuddyConActivity.kt
+++ presentation/src/main/java/com/yapp/buddycon/presentation/ui/main/BuddyConActivity.kt
@@ -16,27 +16,24 @@ import com.yapp.buddycon.presentation.base.BaseActivity
 import com.yapp.buddycon.presentation.databinding.ActivityBuddyConBinding
 import com.yapp.buddycon.presentation.ui.addCoupon.AddCouponActivity
 import com.yapp.buddycon.presentation.ui.makeCoupon.MakeCouponActivity
+import timber.log.Timber
 
 class BuddyConActivity : BaseActivity<ActivityBuddyConBinding>(R.layout.activity_buddy_con) {
 
     private val buddyViewModel: BuddyConViewModel by viewModels()
 
-    private lateinit var type: String // String 변수로 임시 설정 -> 추후 enum으로 변경 예정
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.buddyViewModel = buddyViewModel
-
-        initGallery()
 
         binding.tvMakeCoupon.setOnClickListener {
             startActivity(Intent(this, MakeCouponActivity::class.java))
         }
 
+        initForAddCoupon()
         initToolbar()
         initNavigation()
     }
-
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_buddycon, menu)
@@ -63,24 +60,18 @@ class BuddyConActivity : BaseActivity<ActivityBuddyConBinding>(R.layout.activity
         binding.bottomNavigationView.setupWithNavController(navController)
     }
 
-    private fun initGallery() { // 불필요 주석은 추후 제거 예정
+    private fun initForAddCoupon() { // 불필요 주석은 추후 제거 예정
         val getImageContent =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
                 if (it.resultCode == RESULT_OK) {
-                    Log.d("AppTest", "data : ${it.data}")
-                    //binding.ivPhoto.setImageURI(it.data?.data)
-                    Snackbar.make(binding.root, "사진을 불러왔습니다", Snackbar.LENGTH_SHORT).show()
-                    when (type) {
-                        "add" -> { // 등록
-                            val intent = Intent(this, AddCouponActivity::class.java)
-                            it.data?.let { galleryIntent ->
-                                galleryIntent.data?.let { uri ->
-                                    intent.putExtra("imageUri", uri)
-                                    startActivity(intent)
-                                }
-                            }
+                    //Snackbar.make(binding.root, "사진을 불러왔습니다", Snackbar.LENGTH_SHORT).show()
+
+                    val intent = Intent(this, AddCouponActivity::class.java)
+                    it.data?.let { galleryIntent ->
+                        galleryIntent.data?.let { uri ->
+                            intent.putExtra("imageUri", uri)
+                            startActivity(intent)
                         }
-                        "make" -> {} // 제작
                     }
                 }
             }
@@ -90,7 +81,8 @@ class BuddyConActivity : BaseActivity<ActivityBuddyConBinding>(R.layout.activity
                 ActivityResultContracts.RequestPermission()
             ) { isGranted: Boolean ->
                 if (isGranted) {
-                    Log.e("AppTest", "권한 승인 ok")
+                    Timber.tag("AppTest").e("권한 승인 ok")
+
                     val intent = Intent(Intent.ACTION_PICK) // 갤러리관련 앱
                     //val intent = Intent(Intent.ACTION_GET_CONTENT)  // 전체 이미지 관련 파일 선택 가능한 화면으로 이동
                     intent.type = "image/*"
@@ -100,8 +92,8 @@ class BuddyConActivity : BaseActivity<ActivityBuddyConBinding>(R.layout.activity
                 }
             }
 
-        binding.tvAddCoupon.setOnClickListener { // '쿠폰 등록하기' 선택 시
-            this.type = "add" //
+        // '쿠폰 등록하기' 선택 시
+        binding.tvAddCoupon.setOnClickListener {
             requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
         }
     }
