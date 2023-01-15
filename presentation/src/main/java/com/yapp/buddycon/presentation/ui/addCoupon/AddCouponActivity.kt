5d2e677cfe0a7ac03ac5320f113e49a6594b38e7
한창희 <nimok97@gmail.com>
2023-01-15 15:53:38 +0900

    [Feature] 갤러리에서 선택한 이미지의 바코드 유무 파악 기능 추가

--- presentation/src/main/java/com/yapp/buddycon/presentation/ui/addCoupon/AddCouponActivity.kt
+++ presentation/src/main/java/com/yapp/buddycon/presentation/ui/addCoupon/AddCouponActivity.kt
@@ -3,11 +3,16 @@ package com.yapp.buddycon.presentation.ui.addCoupon
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.common.InputImage
 import com.yapp.buddycon.presentation.R
 import com.yapp.buddycon.presentation.base.BaseActivity
 import com.yapp.buddycon.presentation.databinding.ActivityAddCouponBinding
+import timber.log.Timber
 
 class AddCouponActivity : BaseActivity<ActivityAddCouponBinding>(R.layout.activity_add_coupon) {
+
+    private val TAG = "AppTest"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,14 +22,33 @@ class AddCouponActivity : BaseActivity<ActivityAddCouponBinding>(R.layout.activi
 
     private fun init() {
         setAppbar()
-        getImageUri()
+        getBarcodeNumber()
     }
 
-    private fun getImageUri() {
-        val imgUri = intent.getParcelableExtra<Uri>("imageUri")
-        Log.e("AppTest", "AddCouponActivity/ imgUri : $imgUri")
+    private fun getBarcodeNumber() {
+        val imageUri = intent.getParcelableExtra<Uri>("imageUri")
+        Timber.tag(TAG).e("AddCouponActivity/ imgUri : %s", imageUri)
+        binding.shivCoupon.setImageURI(imageUri)// 현재 테스트용으로 전달받은 이미지 uri로 이미지뷰에 보여주고 있음
 
-        binding.shivCoupon.setImageURI(imgUri) // 현재 테스트용으로 전달받은 이미지 uri로 이미지뷰에 보여주고 있음
+        imageUri?.let { uri ->
+            val barcodeScanner = BarcodeScanning.getClient()
+            val inputImage = InputImage.fromFilePath(this, uri)
+
+            barcodeScanner.process(inputImage)
+                .addOnSuccessListener { barcodes ->
+                    Timber.tag(TAG).e("barcode list size : %s", barcodes.size.toString())
+                    if (barcodes.size >= 1) {
+                        barcodes[0]?.let { barcode ->
+                            val barcodeNumber = barcode.rawValue
+                            Timber.tag(TAG).e("barcode number : %s", barcodeNumber)
+                        }
+                    } else {
+                        Timber.tag(TAG).e("read image success but no barcode")
+                    }
+                }.addOnFailureListener {
+                    Timber.tag(TAG).e("read image fail")
+                }
+        }
     }
 
     private fun setAppbar() {
