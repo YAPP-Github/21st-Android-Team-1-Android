f9a90cabc995a111ede0d7b85053af5f8445671d
ows3090 <ows3090@naver.com>
2023-01-22 00:14:59 +0900

    [Refactor] #35 - UserInfo, UserInfoResponse 데이터 수정

--- data/src/main/java/com/yapp/buddycon/data/network/response/UserInfoResponse.kt
+++ data/src/main/java/com/yapp/buddycon/data/network/response/UserInfoResponse.kt
@@ -2,5 +2,6 @@ package com.yapp.buddycon.data.network.response
 
 // TODO(OWS) : 필드 값 변경
 data class UserInfoResponse(
-    val accessToken: String
+    val accessToken: String,
+    val accessTokenExpiresIn: Int
 )
