8cedb425c9d89c8fb908824783b53a09e805aedb
ows3090 <ows3090@naver.com>
2023-01-24 12:53:01 +0900

    [Feat] Android 12 Splash Screen 설정 및 Toolbar 레이아웃 제작

--- presentation/src/main/java/com/yapp/buddycon/presentation/ui/splash/SplashActivity.kt
+++ presentation/src/main/java/com/yapp/buddycon/presentation/ui/splash/SplashActivity.kt
@@ -1,18 +1,43 @@
 package com.yapp.buddycon.presentation.ui.splash
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.ViewTreeObserver
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.yapp.buddycon.presentation.R
 import com.yapp.buddycon.presentation.base.BaseActivity
 import com.yapp.buddycon.presentation.databinding.ActivitySplashBinding
 import com.yapp.buddycon.presentation.ui.login.KakaoLoginActivity
+import com.yapp.buddycon.presentation.ui.main.BuddyConActivity
 
-// TODO : Android 12 대응
 class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) {
 
+    private var isReady = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, KakaoLoginActivity::class.java))
-        finish()
+        initSplash()
+
+        Handler(mainLooper).postDelayed({
+            isReady = true
+        }, 2000)
+    }
+
+    private fun initSplash(){
+        binding.root.viewTreeObserver.addOnPreDrawListener(
+            object: ViewTreeObserver.OnPreDrawListener{
+                override fun onPreDraw(): Boolean {
+                    return if(isReady){
+                        binding.root.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    }else{
+                        false
+                    }
+                }
+            }
+        )
     }
 }
