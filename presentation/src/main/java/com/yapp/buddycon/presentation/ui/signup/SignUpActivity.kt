6e1963031b3f9eba56d4b6f826cb76774e57db96
ows3090 <ows3090@naver.com>
2023-01-25 18:29:40 +0900

    [Feature] #37 - 회원가입 레이아웃 구현

---
+++ presentation/src/main/java/com/yapp/buddycon/presentation/ui/signup/SignUpActivity.kt
@@ -0,0 +1,14 @@
+package com.yapp.buddycon.presentation.ui.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yapp.buddycon.presentation.R
+import com.yapp.buddycon.presentation.base.BaseActivity
+import com.yapp.buddycon.presentation.databinding.ActivitySignUpBinding
+
+class SignUpActivity : BaseActivity<ActivitySignUpBinding>(R.layout.activity_sign_up) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
