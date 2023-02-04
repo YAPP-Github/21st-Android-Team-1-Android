99792168e336617fdef0d46095127b4ba657347d
ows3090 <ows3090@naver.com>
2023-02-04 17:29:16 +0900

    [Feature] #63 제작티콘, 기프티콘 탭 상태 변경

--- presentation/src/main/java/com/yapp/buddycon/presentation/ui/customcon/CustomConFragment.kt
+++ presentation/src/main/java/com/yapp/buddycon/presentation/ui/customcon/CustomConFragment.kt
@@ -1,8 +1,20 @@
 package com.yapp.buddycon.presentation.ui.customcon
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
 import com.yapp.buddycon.presentation.R
 import com.yapp.buddycon.presentation.base.BaseFragment
 import com.yapp.buddycon.presentation.databinding.FragmentCustomconBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CustomConFragment : BaseFragment<FragmentCustomconBinding>(R.layout.fragment_customcon) {
+
+    private val customConViewModel: CustomConViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = customConViewModel
+    }
 }
--- presentation/src/main/java/com/yapp/buddycon/presentation/ui/giftcon/GiftConFragment.kt
+++ presentation/src/main/java/com/yapp/buddycon/presentation/ui/giftcon/GiftConFragment.kt
@@ -1,8 +1,20 @@
 package com.yapp.buddycon.presentation.ui.giftcon
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
 import com.yapp.buddycon.presentation.R
 import com.yapp.buddycon.presentation.base.BaseFragment
 import com.yapp.buddycon.presentation.databinding.FragmentGiftconBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class GiftConFragment : BaseFragment<FragmentGiftconBinding>(R.layout.fragment_giftcon) {
+
+    private val giftConViewMoel: GiftConViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = giftConViewMoel
+    }
 }
