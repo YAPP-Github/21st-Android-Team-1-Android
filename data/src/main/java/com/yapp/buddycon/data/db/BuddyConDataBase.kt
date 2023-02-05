36f498c434d53d338c4548f5f00e0c9fb106942a
ows3090 <ows3090@naver.com>
2023-02-05 14:38:38 +0900

    [Feature] #65 - Database 정의 및 Local 관련 DB DI 적용

--- data/src/main/java/com/yapp/buddycon/data/db/BuddyConDataBase.kt
+++ data/src/main/java/com/yapp/buddycon/data/db/BuddyConDataBase.kt
@@ -1,4 +1,11 @@
 package com.yapp.buddycon.data.db
 
-class BuddyConDataBase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.yapp.buddycon.data.db.dao.GiftconDao
+import com.yapp.buddycon.data.db.entity.GiftconEntity
+
+@Database(entities = [GiftconEntity::class], version = 1)
+abstract class BuddyConDataBase : RoomDatabase(){
+    abstract fun giftconDao(): GiftconDao
 }
