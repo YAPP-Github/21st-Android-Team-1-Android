5b6ab3920fb3fa535a75381922a814e7d32cdbb5
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2023-02-05 16:42:49 +0900

    [Feat] 기프티콘 불러오기 화면

---
+++ presentation/src/main/java/com/yapp/buddycon/presentation/ui/makeCoupon/GetGiftconActivity.kt
@@ -0,0 +1,10 @@
+package com.yapp.buddycon.presentation.ui.makeCoupon
+
+import com.yapp.buddycon.presentation.R
+import com.yapp.buddycon.presentation.base.BaseActivity
+import com.yapp.buddycon.presentation.databinding.ActivityGetGiftconBinding
+
+class GetGiftConActivity : BaseActivity<ActivityGetGiftconBinding>(R.layout.activity_get_giftcon) {
+
+
+}
