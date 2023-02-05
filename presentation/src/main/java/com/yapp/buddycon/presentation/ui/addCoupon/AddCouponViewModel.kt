8c17fda5e843ddd1baa79340e8f0a10cc586f85d
한창희 <nimok97@gmail.com>
2023-02-06 00:43:43 +0900

    [Feature] 입력값 검사를 위한 EditText TextWatcher 추가 및 material date picker 연결

--- presentation/src/main/java/com/yapp/buddycon/presentation/ui/addCoupon/AddCouponViewModel.kt
+++ presentation/src/main/java/com/yapp/buddycon/presentation/ui/addCoupon/AddCouponViewModel.kt
@@ -2,16 +2,21 @@ package com.yapp.buddycon.presentation.ui.addCoupon
 
 import androidx.lifecycle.ViewModel
 import com.yapp.buddycon.domain.model.CouponInfo
+import com.yapp.buddycon.domain.usecase.addcoupon.GetGifticonInfoByBarcodeUseCase
+import com.yapp.buddycon.domain.usecase.addcoupon.GetMakeconInfoByBarcodeUseCase
 import com.yapp.buddycon.presentation.ui.addCoupon.state.ContentInputState
 import com.yapp.buddycon.presentation.ui.addCoupon.state.CouponInfoLoadState
+import com.yapp.buddycon.presentation.utils.Logging
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class AddCouponViewModel @Inject constructor() : ViewModel() {
+class AddCouponViewModel @Inject constructor(
+    private val getGifticonInfoByBarcodeUseCase: GetGifticonInfoByBarcodeUseCase,
+    private val getMakeconInfoByBarcodeUseCase: GetMakeconInfoByBarcodeUseCase
+) : ViewModel() {
     private val _couponInfoLoadState = MutableStateFlow<CouponInfoLoadState<CouponInfo>>(CouponInfoLoadState.Init)
     val couponInfoLoadState = _couponInfoLoadState.asStateFlow()
 
@@ -24,8 +29,10 @@ class AddCouponViewModel @Inject constructor() : ViewModel() {
 
     // 서버에 바코드번호 존재하는지
     fun checkBarcodeInfo(barcodeNumber: String) {
+
     }
 
+    // 입력 정보로 쿠폰 등록
     fun addCoupon() {
 
     }
@@ -33,9 +40,32 @@ class AddCouponViewModel @Inject constructor() : ViewModel() {
     fun checkTitle(title: String) {
         with(title) {
             titleState.isTitleNormal = !(isEmpty() && length > 16)
-            Timber.tag("AppTest").e("title : $title")
         }
     }
+
+    fun checkStoreName(storeName: String) {
+        with(storeName) {
+            titleState.isStoreNameNormal = !(isEmpty() && length > 16)
+        }
+    }
+
+    fun checkSentMemberName(sentMemberName: String) {
+        with(sentMemberName) {
+            titleState.isSentMemberNameNormal = !(isEmpty() && length > 16)
+        }
+    }
+
+    fun checkMemo(memo: String) {
+        with(memo) {
+            titleState.isMemoNormal = !(isEmpty() && length > 50)
+            Logging.error("title : $memo")
+        }
+    }
+
+    fun setExipireDate(date: String) {
+
+    }
+
 
     // temp
     fun setCouponInfoLoadState() {
