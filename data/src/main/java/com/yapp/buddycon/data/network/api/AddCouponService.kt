0485d6ba68665d6ef98742e011948a728d0ec4e9
한창희 <nimok97@gmail.com>
2023-02-12 16:09:58 +0900

    [Feature] 기프티콘, 제작티콘 정보 조회 api service 추가

--- data/src/main/java/com/yapp/buddycon/data/network/api/AddCouponService.kt
+++ data/src/main/java/com/yapp/buddycon/data/network/api/AddCouponService.kt
@@ -1,4 +1,19 @@
 package com.yapp.buddycon.data.network.api
 
+import com.yapp.buddycon.data.network.response.CustomCouponInfoResponse
+import com.yapp.buddycon.data.network.response.GifticonInfoResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface AddCouponService {
+    @GET("api/v1/coupon/gifticon/info") // 바코드 넘버로 기프티콘 정보 조회하기
+    suspend fun getGifticonInfo(
+        @Query("barcode") barcodeNumber: String
+    ): Response<GifticonInfoResponse>
+
+    @GET("api/v1/coupon/custom-coupon/info")
+    suspend fun getCustomCouponInfo(
+        @Query("barcode") barcodeNumber: String
+    ): Response<CustomCouponInfoResponse>
 }
---
+++ data/src/main/java/com/yapp/buddycon/data/network/response/CustomCouponInfoResponse.kt
@@ -0,0 +1,34 @@
+package com.yapp.buddycon.data.network.response
+
+import com.google.gson.annotations.SerializedName
+import com.yapp.buddycon.domain.model.CouponInputInfo
+
+data class CustomCouponInfoResponse(
+    @SerializedName("id")
+    val id: Int = -1,
+    @SerializedName("imageUrl")
+    val imageUrl: String = "",
+    @SerializedName("barcode")
+    val barcode: String = "",
+    @SerializedName("name")
+    val name: String = "",
+    @SerializedName("expireDate")
+    val expireDate: String = "",
+    @SerializedName("storeName")
+    val storeName: String = "",
+    @SerializedName("sentMemberName")
+    val sentMemberName: String = "",
+    @SerializedName("memo")
+    val memo: String = ""
+) {
+    fun mapToCouponInputInfo() = CouponInputInfo(
+        id = this.id,
+        imageUrl = this.imageUrl,
+        barcode = barcode,
+        name = name,
+        expireDate = expireDate,
+        storeName = storeName,
+        sentMemberName = sentMemberName,
+        memo = memo
+    )
+}
