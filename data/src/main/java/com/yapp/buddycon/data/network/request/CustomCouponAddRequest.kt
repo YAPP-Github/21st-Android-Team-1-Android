123bfb055493122cfb5a0b59486c42a9c3f8134e
한창희 <nimok97@gmail.com>
2023-02-19 20:43:43 +0900

    [Fix] 제작티콘 요청을 위한 data class 수정 & mapper 수정

--- data/src/main/java/com/yapp/buddycon/data/network/request/CustomCouponAddRequest.kt
+++ data/src/main/java/com/yapp/buddycon/data/network/request/CustomCouponAddRequest.kt
@@ -1,12 +1,10 @@
 package com.yapp.buddycon.data.network.request
 
-import java.time.LocalDate
-
 data class CustomCouponAddRequest(
     val sharedCouponId: Long? = null,
     val barcode: String = "",
     val name: String = "",
-    val expireDate: LocalDate = LocalDate.of(2023, 1, 1),
+    val expireDate: String = "",
     val storeName: String = "",
     val memo: String = "",
     val sentMemberName: String = ""
