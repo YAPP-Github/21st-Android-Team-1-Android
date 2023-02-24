2271796c6fc5551312c3ce3152558b1bb1177bad
ows3090 <ows3090@naver.com>
2023-02-24 18:15:35 +0900

    [Feature] #106 - 제작티콘 화면 레이아웃 구현

--- presentation/src/main/java/com/yapp/buddycon/presentation/ui/customcon/CustomConDetailActivity.kt
+++ presentation/src/main/java/com/yapp/buddycon/presentation/ui/customcon/CustomConDetailActivity.kt
@@ -4,17 +4,24 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.yapp.buddycon.presentation.R
 import com.yapp.buddycon.presentation.base.BaseActivity
 import com.yapp.buddycon.presentation.databinding.ActivityCustomConDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class CustomConDetailActivity :
-    BaseActivity<ActivityCustomConDetailBinding>(R.layout.activity_custom_con_detail) {
+class CustomConDetailActivity : BaseActivity<ActivityCustomConDetailBinding>(R.layout.activity_custom_con_detail) {
+
+    private val customConId by lazy { intent?.getIntExtra(CUSTOMCON_ID, 0) ?: 0 }
+    private val customConUsable by lazy { intent?.getBooleanExtra(CUSTOMCON_USABLE, false) ?: false }
+    private val customConDetailViewModel: CustomConDetailViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.customConId = customConId
+        binding.customConUsable = customConUsable
+        binding.customConDetailViewModel = customConDetailViewModel
     }
 
     companion object{
---
+++ presentation/src/main/java/com/yapp/buddycon/presentation/ui/customcon/CustomConDetailViewModel.kt
@@ -0,0 +1,12 @@
+package com.yapp.buddycon.presentation.ui.customcon
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class CustomConDetailViewModel @Inject constructor(
+
+): ViewModel(){
+}
